91c5b06ce022860f624d01e7f55294ef86be9578
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-05 00:16:16 -0300

    module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserManagementModule } from './modules/user-management/user-management.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '@shared/infrastructure/config/app-data-source';
 
 @Module({
-  imports: [UserManagementModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => AppDataSource.options,
+    }),
+    UserManagementModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
